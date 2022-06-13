61f0d63ef2cfb4417421260e0224e51081e23024
RATAN1118 <ratanvk4@gmail.com>
2022-06-13 13:01:14 +0530

    3rd

--- test_seleniumpy.py
+++ test_seleniumpy.py
@@ -3,19 +3,22 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def test_setup():
+def setup_fixture():
     global driver
     driver = webdriver.Chrome("C:\\Users\\GSC-30966\\Downloads\\chromedriver_win32\\chromedriver.exe")
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
     driver.maximize_window()
+    yield
+    driver.close()
+    driver.quit()
 
 
 @pytest.mark.parametrize(
     "username, password",
-    [("standard_user", "secret_sauce"), ("locked_out_user", "secret_sauce")]
+    [("standard_user", "secret_sauce")]
 
 )
-def test_login(test_setup, username, password):
+def test_login( username, password):
     driver.get("https://www.saucedemo.com/")
     driver.find_element_by_id("user-name").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
@@ -24,7 +27,18 @@ def test_login(test_setup, username, password):
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
 
-def test_close():
-    driver.close()
-    driver.quit()
-    print("test completed")
+@pytest.mark.parametrize(
+    "username, password",
+    [ ("locked_out_user", "secret_sauce")]
+
+)
+def test_invalidlogin( username, password):
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element_by_id("user-name").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("login-button").click()
+
+    assert driver.current_url != "https://www.saucedemo.com/inventory.html"
+
+
+
